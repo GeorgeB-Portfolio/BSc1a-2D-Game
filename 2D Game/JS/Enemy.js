065947ce2decb8a0df057f451ca2f57460a73ef2
George Baker <79011517+GeorgeB-Portfolio@users.noreply.github.com>
2022-11-02 12:06:52 +0000

    Add files via upload

---
+++ 2D Game/JS/Enemy.js
@@ -0,0 +1,52 @@
+var enemyHealth = 100;
+var enemyDamage = 25;
+function Enemypreload (scene)
+{
+    scene.load.spritesheet('enemy', 'Assets/Enemy.png', { frameWidth: 64, frameHeight: 64 });
+    
+}
+
+function Enemycreate (scene)
+{
+    enemy = scene.physics.add.sprite(200, 300, 'enemy', [46])
+    
+}
+
+function Enemyupdate (scene)
+{
+    targetX = player.x
+    targetY = player.y
+
+    //Moving up
+    if (targetY < enemy.y)
+    {
+        enemy.setVelocityY(-200);
+
+    }
+    //Moving down
+    else if ( targetY > enemy.y)
+    {
+        enemy.setVelocityY(200);
+
+    }
+    //Moving left
+    if (targetX < enemy.x)
+    {
+        enemy.setVelocityX(-200);
+
+    }
+    //Moving right
+    else if (targetX > enemy.x)
+    {
+        enemy.setVelocityX(200);
+        
+    }
+
+    else
+    {
+        enemy.setVelocityX(0)
+        enemy.setVelocityY
+    }
+
+
+}
---
+++ 2D Game/JS/player.js
@@ -0,0 +1,57 @@
+var playerHealth = 100;
+var playerDamage = 50;
+var playerHealthUI;
+
+function Playerpreload (scene)
+{
+    scene.load.spritesheet('player', 'Assets/Player.png', { frameWidth: 64, frameHeight: 64 });
+}
+
+
+function Playercreate (scene)
+{
+    player = scene.physics.add.sprite(400, 300, 'player', [46])
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    cursors = scene.input.keyboard.createCursorKeys();
+    //Displaying the health of the player
+    playerHealthUI = scene.add.text(10, 10, 'Health: ' + playerHealth, { fontSize: '25px', fill: 'black' });
+
+}
+
+function Playerupdate ()
+{
+    //Moving left
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-200);
+
+    }
+    //Moving right
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(200);
+
+    }
+    //Moving up
+    else if (cursors.up.isDown)
+    {
+        player.setVelocityY(-200);
+
+    }
+    //Moving down
+    else if (cursors.down.isDown)
+    {
+        player.setVelocityY(200);
+
+    }
+    //When none are pressed (not moving)
+    else
+    {
+        player.setVelocityX(0);
+        player.setVelocityY(0);
+
+    }
+
+
+}
